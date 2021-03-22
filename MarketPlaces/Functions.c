cc59038bcaa802e5c9e74ec1c32b67ddc7e7c93d
Antal József <76700822+antaljozsef@users.noreply.github.com>
2021-03-22 17:16:23 +0200

    Proba

---
+++ MarketPlaces/Functions.c
@@ -0,0 +1,5 @@
+//
+// Created by Antal József on 2021. 03. 22..
+//
+
+#include "Functions.h"
---
+++ MarketPlaces/Functions.h
@@ -0,0 +1,42 @@
+//
+// Created by Antal József on 2021. 03. 22..
+//
+
+#ifndef MARKETPLACES_FUNCTIONS_H
+#define MARKETPLACES_FUNCTIONS_H
+
+enum Position{
+        DIRECTOR = 0,
+        MANAGER = 1,
+        SECRETARY = 2,
+        SELLER = 3,
+        LOADER = 4,
+        CLEANER = 5
+};
+
+enum TypeOfMarket{
+        MIXED_MARKET = 0,
+        FOOD_MARKET = 1,
+        TECH_MARKET = 2,
+        CONSTRUCTION_MARKET = 3,
+        OTHER = 4
+};
+
+typedef struct {
+    char firstName[20];
+    char lastName[20];
+    int yearOfBirth;
+    int yearOfExperience;
+    enum Position position;
+}Employees;
+
+typedef struct {
+    char  marketplaceName[20];
+    char  location[20];
+    int numOfEmployees;
+    int marketID;
+    enum Employees* employees;
+    enum TypeOfMarket type;
+}Marketplace;
+
+#endif //MARKETPLACES_FUNCTIONS_H
