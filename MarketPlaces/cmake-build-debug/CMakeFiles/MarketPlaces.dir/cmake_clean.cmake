461a0f06a51d7e1b9a681f5bea06260723365b6f
Antal József <76700822+antaljozsef@users.noreply.github.com>
2021-03-22 18:46:32 +0200

    Print

---
+++ MarketPlaces/cmake-build-debug/CMakeFiles/MarketPlaces.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MarketPlaces.dir/Functions.c.obj"
+  "CMakeFiles/MarketPlaces.dir/main.c.obj"
+  "MarketPlaces.exe"
+  "MarketPlaces.exe.manifest"
+  "MarketPlaces.pdb"
+  "libMarketPlaces.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MarketPlaces.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MarketPlaces/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "Functions.h"
+
+int main() {
+
+    Marketplace * marketplace = createMarket("import.txt");
+    printMarket(marketplace);
+    
+    return 0;
+}
